d588cdc48d5f4a41b50ea6ba0b75a34057a66182
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-07-16 20:07:39 +0200

    Added DB migrations support.

--- scuevals_api/__init__.py
+++ scuevals_api/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -12,6 +13,7 @@ api = Api(app)
 CORS(app)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import scuevals_api.cmd
 import scuevals_api.api
--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Flask-Cors==3.0.3',
+        'Flask-Migrate==2.0.4',
         'Flask-RESTful==0.3.6',
         'Flask-SQLAlchemy==2.2',
         'Flask==0.12.2',
