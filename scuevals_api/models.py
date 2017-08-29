82dc1e29dfdf2c5ab103fe3d17e07f148d92749f
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-08-28 23:13:20 -0400

    Update db models to reflect Section addition

--- scuevals_api/models.py
+++ scuevals_api/models.py
@@ -2,13 +2,9 @@ from scuevals_api import db
 from sqlalchemy.dialects.postgresql import ranges, ExcludeConstraint
 
 
-quarter_course = db.Table('quarter_course', db.metadata,
-                          db.Column('quarter_id', db.Integer, db.ForeignKey('quarter.id')),
-                          db.Column('course_id', db.Integer, db.ForeignKey('course.id')),
-                          db.UniqueConstraint('quarter_id', 'course_id', name='quarter_course_uix'))
-
-
 class University(db.Model):
+    __tablename__ = 'universities'
+
     id = db.Column(db.Integer, primary_key=True)
     abbreviation = db.Column(db.Text, nullable=False, unique=True)
     name = db.Column(db.Text, nullable=False)
@@ -16,95 +12,124 @@ class University(db.Model):
     students = db.relationship('Student', back_populates='university')
     professors = db.relationship('Professor', back_populates='university')
     schools = db.relationship('School', back_populates='university')
+    quarters = db.relationship('Quarter', back_populates='university')
 
 
 class Student(db.Model):
+    __tablename__ = 'students'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.Text, unique=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text)
     graduation_year = db.Column(db.Integer, nullable=False)
-    university_id = db.Column(db.Integer, db.ForeignKey('university.id'), nullable=False)
+
+    university_id = db.Column(db.Integer, db.ForeignKey('universities.id'), nullable=False)
 
     university = db.relationship('University', back_populates='students')
     evaluations = db.relationship('Evaluation', back_populates='student')
 
 
 class Professor(db.Model):
+    __tablename__ = 'professors'
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text)
-    university_id = db.Column(db.Integer, db.ForeignKey('university.id'), nullable=False)
+
+    university_id = db.Column(db.Integer, db.ForeignKey('universities.id'), nullable=False)
 
     university = db.relationship('University', back_populates='professors')
-    evaluations = db.relationship('Evaluation', back_populates='professor')
+    sections = db.relationship('Section', back_populates='professor')
 
 
 class Quarter(db.Model):
+    __tablename__ = 'quarters'
+
     id = db.Column(db.Integer, primary_key=True)
     year = db.Column(db.Integer, nullable=False)
     name = db.Column(db.Text, nullable=False)
     current = db.Column(db.Boolean, default=False)
     period = db.Column(ranges.DATERANGE, nullable=False)
 
-    evaluations = db.relationship('Evaluation', back_populates='quarter')
-    courses = db.relationship('Course', secondary=quarter_course, back_populates="quarters")
+    university_id = db.Column(db.Integer, db.ForeignKey('universities.id'), nullable=False)
+
+    sections = db.relationship('Section', back_populates='quarter')
+    university = db.relationship('University', back_populates='quarters')
 
     __table_args__ = (
         ExcludeConstraint(('period', '&&')),
-        db.UniqueConstraint('year', 'name'),
+        db.UniqueConstraint('year', 'name', 'university_id'),
         db.CheckConstraint(name.in_(['Fall', 'Winter', 'Spring', 'Summer']))
     )
 
 
 class Evaluation(db.Model):
+    __tablename__ = 'evaluations'
+
     id = db.Column(db.Integer, primary_key=True)
 
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'), nullable=False)
-    quarter_id = db.Column(db.Integer, db.ForeignKey('quarter.id'), nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    section_id = db.Column(db.Integer, db.ForeignKey('sections.id'), nullable=False)
 
     student = db.relationship('Student', back_populates='evaluations')
-    professor = db.relationship('Professor', back_populates='evaluations')
-    quarter = db.relationship('Quarter', back_populates='evaluations')
-    course = db.relationship('Course', back_populates='evaluations')
+    section = db.relationship('Section', back_populates='evaluations')
 
-    __table_args__ = (
-        db.UniqueConstraint('student_id', 'professor_id', 'quarter_id', 'course_id', name='uix_evaluation'),
-    )
+    __table_args__ = (db.UniqueConstraint('student_id', 'section_id'),)
 
 
 class Department(db.Model):
+    __tablename__ = 'departments'
+
     id = db.Column(db.Integer, primary_key=True)
     abbreviation = db.Column(db.Text, nullable=False)
     name = db.Column(db.Text, nullable=False)
-    school_id = db.Column(db.Integer, db.ForeignKey('school.id'), nullable=False)
+    school_id = db.Column(db.Integer, db.ForeignKey('schools.id'), nullable=False)
 
     courses = db.relationship('Course', back_populates='department')
     school = db.relationship('School', back_populates='departments')
 
-    __table_args__ = (db.UniqueConstraint('abbreviation', 'school_id', name='uix_department'),)
+    __table_args__ = (db.UniqueConstraint('abbreviation', 'school_id'),)
 
 
 class School(db.Model):
+    __tablename__ = 'schools'
+
     id = db.Column(db.Integer, primary_key=True)
     abbreviation = db.Column(db.Text, nullable=False, unique=True)
     name = db.Column(db.Text, nullable=False)
-    university_id = db.Column(db.Integer, db.ForeignKey('university.id'), nullable=False)
+    university_id = db.Column(db.Integer, db.ForeignKey('universities.id'), nullable=False)
 
     university = db.relationship('University', back_populates='schools')
     departments = db.relationship('Department', back_populates='school')
 
 
 class Course(db.Model):
+    __tablename__ = 'courses'
+
     id = db.Column(db.Integer, primary_key=True)
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=False)
     number = db.Column(db.Text, nullable=False)
     title = db.Column(db.Text, nullable=False)
 
     department = db.relationship('Department', back_populates='courses')
-    evaluations = db.relationship('Evaluation', back_populates='course')
-    quarters = db.relationship('Quarter', secondary=quarter_course, back_populates="courses")
+    sections = db.relationship('Section', back_populates='course')
 
     __table_args__ = (db.UniqueConstraint('department_id', 'number', name='uix_course'),)
+
+
+class Section(db.Model):
+    __tablename__ = 'sections'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    quarter_id = db.Column(db.Integer, db.ForeignKey('quarters.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professors.id'), nullable=False)
+
+    quarter = db.relationship('Quarter', back_populates='sections')
+    course = db.relationship('Course', back_populates='sections')
+    professor = db.relationship('Professor', back_populates='sections')
+    evaluations = db.relationship('Evaluation', back_populates='section')
+
+    __table_args__ = (db.UniqueConstraint('professor_id', 'quarter_id', 'course_id'),)
