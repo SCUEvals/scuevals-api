8ffd6cee9a029056dd9b58fb26a383fda3807926
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-09-02 16:36:57 -0400

    Fixed update_departments

--- db/functions/update_departments.sql
+++ db/functions/update_departments.sql
@@ -15,21 +15,21 @@ begin
   from jsonb_array_elements(_json -> 'results') department
   loop
     -- get the school id
-    select school.id
+    select id
     into _s_id
-    from school
+    from schools
     where abbreviation = _school;
 
     -- get the department id if it exists
     select id
     into _d_id
-    from department
+    from departments
     where school_id = _s_id and abbreviation = _abbr;
 
     -- if department does not exist, create it
     if _d_id is null
     then
-      insert into department (abbreviation, name, school_id) values (_abbr, _name, _s_id);
+      insert into departments (abbreviation, name, school_id) values (_abbr, _name, _s_id);
     end if;
 
   end loop;
