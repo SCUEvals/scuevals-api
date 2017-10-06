f018f827fba9220f0acf883c42ba33bdf4d3d825
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-10-06 16:34:53 -0700

    Add user logic

--- scuevals_api/auth.py
+++ scuevals_api/auth.py
@@ -5,6 +5,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_simple import create_jwt, decode_jwt
 from webargs.flaskparser import use_kwargs
 from webargs import missing, fields
+from scuevals_api.models import Student, db
 from scuevals_api.errors import BadRequest, Unauthorized
 
 auth_bp = Blueprint('auth', __name__)
@@ -29,15 +30,52 @@ def auth(id_token):
         raise BadRequest('invalid id_token')
 
     if float(data['exp']) < time.time():
-        raise BadRequest('inavlid id_token')
+        raise BadRequest('invalid id_token')
 
     # TODO: Get this value from the database
+    # essentially, the only way to figure out which university
+    # the student is allowed/supposed to sign up for, is to check
+    # which domain the request came from (needs to be HTTPS as well)
+    # there also needs to be a debug mode where this verification is skipped
     if data['hd'] != 'scu.edu':
-        raise BadRequest('inavlid id_token')
+        raise BadRequest('invalid id_token')
 
-    jwt = create_jwt(identity=data['email'])
+    user = Student.query.filter_by(email=data['email']).one()
 
-    return jsonify({'jwt': jwt})
+    if user is None:
+        # new user
+        status = 'new'
+
+        user = Student(
+            email=data['email'],
+            first_name=data['given_name'],
+            last_name=data['family_name'],
+            university_id=1
+        )
+
+        db.session.add(user)
+        db.session.flush()
+    else:
+        # check if user is complete
+        if user.graduation_year is None:
+            status = 'incomplete'
+            user.first_name = data['given_name']
+            user.last_name = data['family_name']
+        else:
+            status = 'ok'
+
+    ident = {
+        'id': user.id,
+        'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name
+    }
+
+    jwt = create_jwt(identity=ident)
+
+    db.session.commit()
+
+    return jsonify({'status': status, 'jwt': jwt})
 
 
 @auth_bp.route('/auth/validate', methods=['POST'])
