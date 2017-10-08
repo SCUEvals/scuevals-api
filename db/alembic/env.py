d0441696d8117eb45e40399b1c362a36f2c5e3e1
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-10-08 12:26:54 -0700

    Remove alembic autogenerate support

--- db/alembic/env.py
+++ db/alembic/env.py
@@ -3,7 +3,6 @@ import os
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-from scuevals_api.models import db
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +18,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = db.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
