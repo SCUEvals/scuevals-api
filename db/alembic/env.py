a2ad10d4beff8d375a995656f9323154fb89d367
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-10-24 18:02:31 -0700

    Add app to alembic env

--- db/alembic/env.py
+++ db/alembic/env.py
@@ -1,8 +1,13 @@
 from __future__ import with_statement
 import os
+import sys
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+
+sys.path.append(os.getcwd())
+
+from scuevals_api.models import db  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +23,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
