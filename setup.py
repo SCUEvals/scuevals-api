7738c686d31d72e89fb6555fa6874885c5dcd458
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-10-27 15:47:52 -0700

    Add missing requirement

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         'beautifulsoup4==4.6.0',
         'blinker==1.4',
+        'coveralls==1.2.0',
         'Flask-Caching==1.3.3',
         'Flask-Cors==3.0.3',
         'Flask-JWT-Extended==3.3.4',
