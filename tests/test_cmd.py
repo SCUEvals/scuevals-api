7db725992c5edc064f806d36540292b8c0b1a219
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2017-11-06 09:16:34 -0800

    Simplify CLI tests

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,8 +1,6 @@
 import unittest
 from click.testing import CliRunner
-from flask.cli import ScriptInfo
 
-from scuevals_api import create_app
 from scuevals_api.cmd import cli
 
 
@@ -11,10 +9,9 @@ class CmdsTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.runner = CliRunner()
-        obj = ScriptInfo(create_app=create_app)
 
         def cli_run(self, *cmds):
-            return self.runner.invoke(cli, cmds, obj=obj)
+            return self.runner.invoke(cli, cmds)
 
         cls.cli_run = cli_run
 
