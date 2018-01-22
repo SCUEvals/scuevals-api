390f0272d024f404245d4394e3f8994ec5f3077c
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2018-01-21 23:32:30 -0800

    Fix missing comma

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'gunicorn==19.7.1',
         'newrelic==2.100.0.84',
         'psycopg2==2.7.3.2',
-        'pycryptodome>=3.4.7'
+        'pycryptodome>=3.4.7',
         'python-jose==2.0.1',
         'PyYAML==3.12',
         'requests==2.18.4',
