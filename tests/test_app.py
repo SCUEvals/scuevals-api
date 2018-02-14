225d57af2e89a5e7d0058e8a4d44e540f16f7d0c
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2018-02-13 23:36:38 -0800

    Prevent rollbar during tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,8 @@ class AppTestCase(unittest.TestCase):
     def test_create_app(self):
         create_app()
 
-    @mock.patch('rollbar.init_app', create=True, return_value=True)
-    def test_create_app_production(self, init_app_func):
+    @mock.patch('scuevals_api.errors.rollbar.init_app', create=True, return_value=True)
+    def test_create_app_production(self, rollbar_init_func):
         os.environ['DATABASE_URL'] = os.environ['TEST_DATABASE_URL']
         create_app('production')
+        self.assertTrue(rollbar_init_func.called)
