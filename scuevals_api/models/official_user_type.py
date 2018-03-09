3b3455e256b749369d17c0a93a4205a7d0039e6c
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2018-03-09 13:24:44 -0800

    Add support for faculty accounts

--- scuevals_api/models/official_user_type.py
+++ scuevals_api/models/official_user_type.py
@@ -10,3 +10,7 @@ class OfficialUserType(db.Model):
     university_id = db.Column(db.Integer, db.ForeignKey('universities.id'), nullable=False)
 
     university = db.relationship('University', back_populates='official_user_types')
+
+    __table_args__ = (
+        db.UniqueConstraint('email', 'type', 'university_id'),
+    )
